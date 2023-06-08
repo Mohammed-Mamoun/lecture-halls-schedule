b2659d0f9aeb82fc37f64d93efaee081988bb9fa
Mohammed-Mamoun <86966623+Mohammed-Mamoun@users.noreply.github.com>
2023-06-08 21:48:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ //this page was created by: Abdullah Alqahtani ,Hatem Alsehli, Hussam Alelyani, Mohammed Abdulghani,Saleh Alkharboush, Mohammed Marrouki//  
+   
+   function changeContent(title, imagePath) {
+        document.getElementById('title').innerHTML = title;
+        document.getElementById('main-image').src = imagePath;
+    }
+    
